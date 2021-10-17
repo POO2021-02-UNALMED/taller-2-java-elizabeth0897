924494d6938a9a1bda3f3e774cb68b362d7ec9a9
Elizabeth <elramirezo@unal.edu.co>
2021-10-17 03:24:46 -0500

    Taller 2

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,4 +1,33 @@
-
+package test;
 public class Auto {
-
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int n = 0;
+		for (int i = 0; i < asientos.length; i++) {
+			if (asientos[i] != null) {
+				n = +1;
+			}	
+		}
+		return n;
+	}
+	
+	String verificarIntegridad() {
+		if(motor.registro != registro) {
+			return "Las piezas no son originales";
+		} else {
+			for(int i = 0; i< asientos.length; i++) {
+				if(asientos[i] != null && registro != asientos[i].registro) {
+					return "Las piezas no son originales";
+				}
+			}
+		return "Auto original";
+		}
+	}	
 }
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,4 +1,16 @@
-
+package test;
 public class Motor {
-
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro(int numero) {
+		this.registro = numero;
+	}
+	
+	void asignarTipo(String tipoMotor) {
+		if (tipoMotor.equals("gasolina")||tipoMotor.equals("electrico")) {
+			this.tipo = tipoMotor;
+		}
+	}
 }
